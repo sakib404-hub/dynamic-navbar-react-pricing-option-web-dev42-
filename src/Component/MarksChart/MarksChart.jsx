c72dafb88cc1e138d2409f9f9c83370f5da88f67
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-10-05 23:12:29 +0600

    this is how to add bar chart

--- src/Component/MarksChart/MarksChart.jsx
+++ src/Component/MarksChart/MarksChart.jsx
@@ -1,5 +1,5 @@
 import React, { use } from 'react';
-import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, BarChart, Bar } from 'recharts';
 
 const MarksChart = ({ marksPromise }) => {
 
@@ -16,20 +16,36 @@ const MarksChart = ({ marksPromise }) => {
             chemistry: data.marks.chemistry,
             math: data.marks.math
         }
+
+        const avg = (student.physics + student.chemistry + student.math) / 3;
+        student.avg = avg;
+
         return student;
     })
 
-    console.log(marksChart);
+    // console.log(marksChart);
 
     return (
-        <div className='flex items-center justify-center'>
-            <LineChart width={700} height={400} data={marksChart}>
-                <YAxis></YAxis>
-                <XAxis dataKey={'name'}></XAxis>
-                <Line dataKey={'physics'}></Line>
-                <Line dataKey={'chemistry'} stroke='red'></Line>
-                <Line dataKey={'math'} stroke='green'></Line>
-            </LineChart>
+        <div>
+            <div className='flex items-center justify-center'>
+                <LineChart width={700} height={400} data={marksChart}>
+                    <YAxis></YAxis>
+                    <XAxis dataKey={'name'}></XAxis>
+                    <Line dataKey={'physics'}></Line>
+                    <Line dataKey={'chemistry'} stroke='red'></Line>
+                    <Line dataKey={'math'} stroke='green'></Line>
+                    <Line dataKey={'avg'} stroke='yellow'></Line>
+                </LineChart>
+            </div>
+            <div className='flex items-center justify-center'>
+                <BarChart width={1000} height={600} data={marksChart}>
+                    <XAxis dataKey={'name'} interval={1}></XAxis>
+                    <YAxis></YAxis>
+                    <Bar dataKey={'avg'} fill='yellow'></Bar>
+                    <Bar dataKey={'chemistry'} fill='blue'></Bar>
+                    <Bar dataKey={'physics'} fill='red'></Bar>
+                </BarChart>
+            </div>
         </div>
     );
 };
