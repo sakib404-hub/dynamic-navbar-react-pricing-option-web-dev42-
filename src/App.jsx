29ae1aaffe7d525e05c56e3ef9690ae1cd86d024
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-10-05 18:04:18 +0600

    added the necessary elements and template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Component/Header/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <p>Hellow</p>
+    </div>
+  );
+};
+
+export default App;
